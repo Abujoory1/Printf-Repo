9abb502064a34167932ddc9d1836a6750aaa5ea5
Abujoory1 <mr.musamu@gmail.com>
2023-10-17 11:08:48 -0700

    prints a number

--- printf_integer.c
+++ printf_integer.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * printf_integer - Prints an integer
- * @list: list of arguments
+ * printf_integer - Prints an integer.
+ * @list: list of arguments.
  * Return: Will return the amount of characters printed.
  */
 
@@ -12,4 +12,43 @@ int printf_integer(va_list list)
 
 	num_length = printf_number(list);
 	return (num_length);
+
+}
+
+/**
+ * printf_number - prints a number send to this function
+ * @args: List of arguments
+ * Return: The number of arguments printed
+ */
+
+int printf_number(va_list args)
+{
+	int n;
+	int div;
+	int len;
+	unsigned int num;
+
+	n  = va_arg(args, int);
+	div = 1;
+	len = 0;
+
+	if (n < 0)
+	{
+		len += _putchar('-');
+		num = n * -1;
+	}
+	else
+		num = n;
+
+	for (; num / div > 9;)
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
 }
