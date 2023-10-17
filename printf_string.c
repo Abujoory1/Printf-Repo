7605b7ef69703bfc63d62a848c6c597c6f3c375a
yasermoamd <yasermoamd@hotmail.com>
2023-10-17 04:34:23 -0700

    fixed print_string func

--- printf_string.c
+++ printf_string.c
@@ -1,72 +1,20 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
- * printf_string - print exclusuives string.
- * @val: argumen t.
- * Return: the length of the string.
+ * printf_string - Prints a string
+ * @list: list of arguments
+ * Return: Will return the amount of characters printed.
  */
 
-int printf_string(va_list val)
+int printf_string(va_list list)
 {
-	register short len = 0;
-	char *res, *s = va_arg(val, char *);
-	int count;
+	int i;
+	char *str;
 
-	if (!s)
-		return (_puts(NULL_STRING));
-	for (; *s; s++)
-	{
-		if (isNonAlphaNumeric(*s))
-		{
-			count += _puts("\\x");
-			res = convert(*s, 16, 0);
-			if (!res[1])
-				len += _putchar('0');
-			len += _puts(res);
-		}
-		else
-			len += _putchar(*s);
-	}
-	return (len);
-}
-
-
-/**
- * isNonAlphaNumeric - determines if char is a non-
- * alphanumeric char on ASCII table
- * @c: input char
- * Return: true or false
- */
-
-int isNonAlphaNumeric(char c)
-{
-	return ((c > 0 && c < 32) || c >= 127);
-}
-
-/**
- * convert - converts number and base into string
- * @num: input number
- * @base: input base
- * @lowercase: flag if hexa values need to be lowercase
- * Return: result string
- */
-
-char *convert(unsigned long int num, int base, int lowercase)
-{
-	static char *rep;
-	static char buffer[50];
-	char *ptr;
-
-	rep = (lowercase)
-		? "0123456789abcdef"
-		: "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = NUL;
-	do {
-		*--ptr = rep[num % base];
-		num /= base;
-	} while (num);
-
-	return (ptr);
+	str = va_arg(list, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
 }
