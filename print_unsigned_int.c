0e8b8ac6f3aff8d8f9fb9063d23d77f24849988c
yasermoamd <yasermoamd@hotmail.com>
2023-10-17 22:21:03 -0700

    print a number

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -26,5 +26,26 @@ int printf_unsigned_int(va_list list)
  * Return: length of n.
  */
 
-int printf_unsgined_num(unsigned int n)
-{}
+int print_unsgined_number(unsigned int n)
+{
+	int div;
+	int len;
+	unsigned int num;
+
+	div = 1;
+	len = 0;
+
+	num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
+}
